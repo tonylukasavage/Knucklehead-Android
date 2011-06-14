fcb53501a969b1eb770aff99bb20e8329c2fe7f9
Tony Lukasavage <anthony.lukasavage@gmail.com>
2011-06-14 11:45:44 -0400

    made the profile view scrollable, not just the vital statistics

--- src/com/savagelook/knucklehead/activity/FighterProfileActivity.java
+++ src/com/savagelook/knucklehead/activity/FighterProfileActivity.java
@@ -1,12 +1,9 @@
 package com.savagelook.knucklehead.activity;
 
-import com.savagelook.android.KeyValuePair;
-import com.savagelook.knucklehead.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,14 +19,13 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.ads.AdRequest;
 import com.google.ads.AdView;
+import com.savagelook.knucklehead.R;
 
 public class FighterProfileActivity extends Activity {
 	private AdView adView;
@@ -55,9 +51,8 @@ public class FighterProfileActivity extends Activity {
 	        // set profile data
 	        TextView tvNickname = (TextView)findViewById(R.id.fighter_nickname1);
 	        TextView tvRecord = (TextView)findViewById(R.id.fighter_record_text);
-	        ListView listView = (ListView)findViewById(R.id.profile_listview);
-	        JSONArray profileJsonArray = json.getJSONArray("profile");
-	        ArrayList<KeyValuePair<String,String>> pairs = new ArrayList<KeyValuePair<String,String>>();        
+	        LinearLayout stats = (LinearLayout)findViewById(R.id.vital_stats);
+	        JSONArray profileJsonArray = json.getJSONArray("profile");       
 	        
 	        // set the name, nickname, and record
 	        for (int i = 0; i < profileJsonArray.length(); i++ ) {
@@ -78,13 +73,16 @@ public class FighterProfileActivity extends Activity {
 			        		tvRecord.setText(value);
 		        		}
 		        	} else {
-			        	pairs.add(new KeyValuePair<String,String>(key, value));
+		        		LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			        LinearLayout row = (LinearLayout)vi.inflate(R.layout.tab_profile_row, null);
+			        TextView keyView = (TextView)row.findViewById(R.id.key_text);
+					TextView valueView = (TextView)row.findViewById(R.id.value_text);
+					keyView.setText(key);
+			        valueView.setText(value);
+			        stats.addView(row);
 		        	}
 	        }
-	        
-	        // put the rest of the profile items in the list
-	        listView.setAdapter(new FighterProfileAdapter(this, R.layout.tab_profile_row, pairs)); 
-	        
+
 	        // remove elements from layout if they are blank
 	        if (tvNickname.getText().equals("")) {
 		        	tvNickname.setVisibility(View.GONE);
@@ -127,35 +125,5 @@ public class FighterProfileActivity extends Activity {
         intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
         startActivity( intent );
 	    	return false;
-    }
-    
-    private class FighterProfileAdapter extends ArrayAdapter<KeyValuePair<String,String>> {
-	    	private ArrayList<KeyValuePair<String,String>> pairs;
-	    	
-	    	public FighterProfileAdapter(Context context, int textViewResourceId, ArrayList<KeyValuePair<String,String>> pairs) {
-		    	super(context, textViewResourceId, pairs);
-		    	this.pairs = pairs;
-	    	}
-	    	
-	    	@Override
-	    	public View getView(int position, View convertView, ViewGroup parent) {
-			View v = convertView;
-			if (v == null) {
-				LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	            v = vi.inflate(R.layout.tab_profile_row, null);	
-			}
-			
-			TextView label = (TextView)v.findViewById(R.id.label_text);
-			TextView valueView = (TextView)v.findViewById(R.id.value_text);
-	        label.setText(pairs.get(position).getKey());
-	        valueView.setText(pairs.get(position).getValue());
-			
-			return v;
-	    	}
-	    	
-	    	@Override
-	    	public boolean isEnabled(int position) {
-		    	return false;
-	    	}
     }
 }
