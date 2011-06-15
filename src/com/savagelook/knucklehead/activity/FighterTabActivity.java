7ac802483593e51f8f0b4c65a37f4e1512eda835
Tony Lukasavage <anthony.lukasavage@gmail.com>
2011-06-15 14:50:05 -0400

    added title bar with fighter name to the tab activity

--- src/com/savagelook/knucklehead/activity/FighterTabActivity.java
+++ src/com/savagelook/knucklehead/activity/FighterTabActivity.java
@@ -1,17 +1,46 @@
 package com.savagelook.knucklehead.activity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.savagelook.android.Lazy;
 import com.savagelook.knucklehead.*;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TabHost;
 
 public class FighterTabActivity extends TabActivity {
+	private static final String TAG = "FighterTabActivity";
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fightertab);
+        
+        String jsonString = getIntent().getStringExtra("json");
+        
+        // get fighter name for title bar
+        // TODO - push this logic out to the screen scraping code
+        this.setTitle("Fighter Profile");
+        try {
+	        JSONObject json = new JSONObject(jsonString);
+	        JSONArray profileJsonArray = json.getJSONArray("profile");  
+	        for (int i = 0; i < profileJsonArray.length(); i++ ) {
+		        	JSONObject item = profileJsonArray.getJSONObject(i);
+		        	String key = item.getString("k");
+		        	String value = item.getString("v");
+		        	
+		        	if (key.toLowerCase().equals("name")) {
+		        		this.setTitle("Fighter: " + value);
+		        	}
+	        }
+        } catch (JSONException e) {
+		    Log.e(TAG, Lazy.Ex.getStackTrace(e));
+        }
         
         Resources res = getResources(); // Resource object to get Drawables
         TabHost tabHost = getTabHost();  // The activity TabHost
@@ -20,9 +49,7 @@ public class FighterTabActivity extends TabActivity {
 
         // Create an Intent to launch an Activity for the tab (to be reused)
         intent = new Intent().setClass(this, FighterProfileActivity.class);
-		intent.putExtra("json", getIntent().getStringExtra("json"));
-        
-        // Initialize a TabSpec for each tab and add it to the TabHost
+		intent.putExtra("json", jsonString);
         spec = tabHost.newTabSpec("profile").setIndicator("Profile",
                           res.getDrawable(R.drawable.ic_tab_profile))
                       .setContent(intent);
@@ -30,14 +57,14 @@ public class FighterTabActivity extends TabActivity {
 
         // Do the same for the other tabs
         intent = new Intent().setClass(this, FighterRecordActivity.class);
-        intent.putExtra("json", getIntent().getStringExtra("json"));
+        intent.putExtra("json", jsonString);
         spec = tabHost.newTabSpec("record").setIndicator("Fights",
                           res.getDrawable(R.drawable.ic_tab_record))
                       .setContent(intent);
         tabHost.addTab(spec);
 
         intent = new Intent().setClass(this, FighterGalleryActivity.class);
-        intent.putExtra("json", getIntent().getStringExtra("json"));
+        intent.putExtra("json", jsonString);
         spec = tabHost.newTabSpec("pictures").setIndicator("Gallery",
                           res.getDrawable(R.drawable.ic_tab_pictures))
                       .setContent(intent);
